97d532f8acb56d79acfcebf3749672d472151cee
AlexeyNelyubov <ANelyubov93@gmail.com>
2023-05-06 23:21:39 +0300

    init project

---
+++ src/components/PiniaStore.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+import { useCounterStore } from "/src/stores/PiniaCounter.js";
+
+const counterStore = useCounterStore();
+</script>
+
+<template>
+  <main>
+    <button @click="counterStore.decriment">-</button>
+    <button @click="counterStore.increment">+</button>
+    <button @click="counterStore.doubleCount">x2</button>
+    {{ counterStore.count }}
+  </main>
+</template>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+import PiniaStore from "/src/components/PiniaStore.vue";
+
+const currentStore = ref("");
+</script>
+
+<template>
+  <header>
+    <button @click="currentStore = 'Pinia'">Pinia</button>
+    <button @click="currentStore = 'Vuex'">Vuex</button>
+    <button @click="currentStore = 'Composable'">Composable</button>
+  </header>
+  <main>
+    <PiniaStore v-if="currentStore === 'Pinia'" />
+  </main>
+</template>
